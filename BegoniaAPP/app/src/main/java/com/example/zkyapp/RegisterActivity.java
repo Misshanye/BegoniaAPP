bba34ca4a613ac1fcf2b1b1dc4862ca1f7b816dc
20212 <2021271257@qq.com>
2023-12-18 17:32:25 +0800

    登陆服务与Tomcat进行连接

--- BegoniaAPP/app/src/main/java/com/example/zkyapp/RegisterActivity.java
+++ BegoniaAPP/app/src/main/java/com/example/zkyapp/RegisterActivity.java
@@ -2,12 +2,17 @@ package com.example.zkyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -17,38 +22,92 @@ public class RegisterActivity extends AppCompatActivity {
     RadioButton choice;
     //查看服务说明书
     TextView check;
-    int flag=1;
+    EditText id, psw_1, psw_2, phoneNum;
+    int flag = 1;
+    private int ResultCode = 2;
+    private final static int REGISTER_JUDGE = 2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        quit=findViewById(R.id.quit);
-        choice=findViewById(R.id.choice);
-        check=findViewById(R.id.check);
+        quit = findViewById(R.id.quit);
+        choice = findViewById(R.id.choice);
+        check = findViewById(R.id.check);
+        psw_1 = (EditText) findViewById(R.id.password_edit);
+        psw_2 = (EditText) findViewById(R.id.password_edit1);
+        phoneNum = (EditText) findViewById(R.id.phoneNum);
 
-        RegisterListener lis=new RegisterListener();
+        RegisterListener lis = new RegisterListener();
         quit.setOnClickListener(lis);
         check.setOnClickListener(lis);
         choice.setOnClickListener(lis);
+        psw_1.setOnClickListener(lis);
+        psw_2.setOnClickListener(lis);
+        phoneNum.setOnClickListener(lis);
     }
-    class RegisterListener implements View.OnClickListener{
+
+    class RegisterListener implements View.OnClickListener {
         @Override
         public void onClick(View view) {
-            if(view.getId()==R.id.quit)
-            {
-                startActivity(new Intent(RegisterActivity.this,MainActivity.class));
-            }
-            else if (view.getId()==R.id.check) {
+            if (view.getId() == R.id.quit) {
+                startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+            } else if (view.getId() == R.id.check) {
                 Intent it = new Intent(RegisterActivity.this, DialogActivity.class);
                 startActivity(it);
-            }
-            else if (view.getId()==R.id.choice) {
-                if(flag==1)
+            } else if (view.getId() == R.id.choice) {
+                if (flag == 1)
                     choice.setChecked(true);
                 else
                     choice.setChecked(false);
-                flag=-flag;
+                flag = -flag;
+            } else if (view.getId() == R.id.signUp) {
+                if (!psw_1.getText().toString().equals(psw_2.getText().toString())) {
+                    Toast.makeText(RegisterActivity.this, "两次密码不一致！", Toast.LENGTH_LONG).show();
+                } else {
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            String result = HttpLogin.RegisterByPost(id.getText().toString(),
+                                    psw_1.getText().toString(), phoneNum.getText().toString());
+                            Bundle bundle = new Bundle();
+                            bundle.putString("result", result);
+                            Message msg = new Message();
+                            msg.what = REGISTER_JUDGE;
+                            msg.setData(bundle);
+                            handler.sendMessage(msg);
+                        }
+                    }).start();
+                }
             }
         }
+
+        @SuppressLint("HandlerLeak")
+        Handler handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                switch (msg.what) {
+                    case REGISTER_JUDGE: {
+                        Bundle bundle = new Bundle();
+                        bundle = msg.getData();
+                        String result = bundle.getString("result");
+                        //Toast.makeText(MainActivity.this,result,Toast.LENGTH_SHORT).show();
+                        try {
+                            if (result.equals("success")) {
+                                Intent intent = new Intent();
+                                intent.putExtra("id", id.getText().toString());
+                                intent.putExtra("password", psw_1.getText().toString());
+                                setResult(ResultCode, intent);//向上一级发送数据
+                                finish();
+                            }
+                        } catch (NullPointerException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    break;
+                }
+            }
+        };
     }
 }
--- BegoniaAPP/app/src/main/java/com/example/zkyapp/otherSignUpActivity.java
+++ BegoniaAPP/app/src/main/java/com/example/zkyapp/otherSignUpActivity.java
@@ -2,12 +2,23 @@ package com.example.zkyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+
+import android.os.Message;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.logging.LogRecord;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Bundle;
+import android.widget.Toast;
+
 
 public class otherSignUpActivity extends AppCompatActivity {
     //退回登陆界面
@@ -15,39 +26,92 @@ public class otherSignUpActivity extends AppCompatActivity {
     //选择服务说明书
     RadioButton choice;
     //查看服务说明书
-    TextView check;
-    int flag=1;
+    TextView check, id, password;
+    int flag = 1;
+    private final static int LOGIN_JUDGE = 1;
+    private int RequestCode = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other_sing_up);
-        quit=findViewById(R.id.quit);
-        choice=findViewById(R.id.choice);
-        check=findViewById(R.id.check);
+        quit = findViewById(R.id.quit);
+        choice = findViewById(R.id.choice);
+        check = findViewById(R.id.check);
+        id = findViewById(R.id.id);
+        password = findViewById(R.id.password);
 
-        Listener lis=new Listener();
+        Listener lis = new Listener();
         quit.setOnClickListener(lis);
         check.setOnClickListener(lis);
         choice.setOnClickListener(lis);
+        id.setOnClickListener(lis);
+        password.setOnClickListener(lis);
     }
-    class Listener implements View.OnClickListener{
+
+    class Listener implements View.OnClickListener {
         @Override
         public void onClick(View view) {
-            if(view.getId()==R.id.quit)
-            {
-                startActivity(new Intent(otherSignUpActivity.this,MainActivity.class));
-            }
-            else if (view.getId()==R.id.check) {
+            if (view.getId() == R.id.quit) {
+                startActivity(new Intent(otherSignUpActivity.this, MainActivity.class));
+            } else if (view.getId() == R.id.check) {
                 Intent it = new Intent(otherSignUpActivity.this, DialogActivity.class);
                 startActivity(it);
-            }
-            else if (view.getId()==R.id.choice) {
-                if(flag==1)
+            } else if (view.getId() == R.id.choice) {
+                if (flag == 1)
                     choice.setChecked(true);
                 else
                     choice.setChecked(false);
-                flag=-flag;
+                flag = -flag;
+            }
+            else if(view.getId()==R.id.login)
+            {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        //使用下面类里的函数，连接servlet，返回一个result，使用handler处理这个result
+                        String result = HttpLogin.LoginByPost(id.getText().toString(),password.getText().toString());
+                        Bundle bundle = new Bundle();
+                        bundle.putString("result",result);
+                        Message message = new Message();
+                        message.setData(bundle);
+                        message.what = LOGIN_JUDGE;
+                        handler.sendMessage(message);
+                    }
+                }).start();
             }
         }
     }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == 2) {
+            id.setText(data.getStringExtra("id"));
+            password.setText(data.getStringExtra("password"));
+        }
+    }
+    @SuppressLint("HandlerLeak")
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg){
+            super.handleMessage(msg);
+            switch (msg.what){
+                case LOGIN_JUDGE:{
+                    Bundle bundle = new Bundle();
+                    bundle = msg.getData();
+                    String result = bundle.getString("result");
+                    //Toast.makeText(MainActivity.this,result,Toast.LENGTH_SHORT).show();
+                    try {
+                        if (result.equals("success")) {
+                            Intent it = new Intent(otherSignUpActivity.this, EventOrderActivity.class);
+                            startActivity(it);
+                        }
+                    }catch (NullPointerException e){
+                        e.printStackTrace();
+                    }
+                }
+                break;
+            }
+        }
+    };
+
 }
